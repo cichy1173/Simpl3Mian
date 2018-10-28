17f9998e9fb326f7a3dfa1032270d82102789368
cichy1173 <32648503+cichy1173@users.noreply.github.com>
2018-10-28 14:54:06 +0100

    Add files via upload

---
+++ Simpl3Mian_source_code.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+float funkcjaKwadratowa(float a, float b, float c, float delta, float x1, float x2, float x0);
+main()
+{
+	float a, b, c;
+	float delta;
+	float x1, x2, x0;
+	printf("Podaj wspolczynniki trojmianu a, b i c\n");
+	scanf("%f %f %f", &a, &b, &c);
+	funkcjaKwadratowa(a, b, c, delta, x1, x2, x0);
+	system("pause");
+	return 0;
+
+}
+float funkcjaKwadratowa(float a, float b, float c, float delta, float x1, float x2, float x0) {
+	if (a!=0) {
+	delta=b*b-4*a*c;
+		if (delta>0) {
+	x1=(-b-sqrt(delta))/(2*a);
+	x2=(-b+sqrt(delta))/(2*a);
+	printf("Miejsca zerowe to: %.2f %.2f", x1, x2); } else
+	if (delta==0)
+	x0=(-b)/(2*a);
+	else {
+		printf("Brak miejsc zerowych\n");
+	}
+}
+    if (a==0 && b!=0 && c!=0) {
+    	x0=(-c)/b;
+    	printf("Podwojny pierwiastek to: %.2f", x0);
+	}
+
+    	if (a==0 && b==0 && c!=0) {
+    		printf("Brak miejsc zerowych funkcji");
+		}
+
+		if (a==0 && b==0 && c==0) {
+		printf("Nieskonczenie wiele pierwiastkow");
+		}
+
+
+	}
